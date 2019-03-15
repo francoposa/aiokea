221d35883b46aa90ab4dd2b59ffb4473bc4ba4ad
Franco Posa <franco@telnyx.com>
2019-03-15 16:45:17 -0500

    added startup handler to register dependencies prior to http server startup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 import os
+from typing import Mapping
 
 from aiohttp import web
 
 from app.infrastructure.server import http
 
+def on_startup(conf: Mapping):
+    """Return a startup handler that will perform background tasks"""
+
+    async def startup_handler(app: web.Application) -> None:
+        """Run all initialization tasks.
+
+        These are tasks that should be run after the event loop has been started but before the HTTP
+        server has been started.
+        """
+        pass
+
+    return startup_handler
 
 def main():
     app = web.Application()
