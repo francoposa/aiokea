2c66ec0de46a6bf691a697f9fbb303cc9fac8297
Franco Posa <f2j21p94@gmail.com>
2019-04-01 19:41:01 -0500

    WIP

---
+++ app/infrastructure/datastore/postgres/tables/user.py
@@ -0,0 +1,21 @@
+"""User table"""
+
+import sqlalchemy as sa
+
+from app.infrastructure.datastore.postgres.tables.metadata import METADATA
+
+USER = sa.Table(
+    "users",
+    METADATA,
+    sa.Column("id", sa.String, primary_key=True),
+    sa.Column("username", sa.String, unique=True),
+    sa.Column("email", sa.String),
+    sa.Column("created_at", sa.DateTime, nullable=False, server_default=sa.func.now()),
+    sa.Column(
+        "updated_at",
+        sa.DateTime,
+        nullable=False,
+        server_default=sa.func.now(),
+        server_onupdate=sa.func.now(),
+    ),
+)
