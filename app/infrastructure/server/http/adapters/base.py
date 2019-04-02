339b161adbacad89984e37a92d45826c0105487d
Franco Posa <f2j21p94@gmail.com>
2019-04-01 20:38:42 -0500

    adding in beginning of handlers

--- app/infrastructure/server/http/adapters/base.py
+++ app/infrastructure/server/http/adapters/base.py
@@ -1,21 +1,21 @@
-# import attr
-# from marshmallow import Schema, ValidationError
-#
-#
-# class BaseJSONUsecaseAdapter:
-#     """Class to marshal JSON into attrs classes using marshmallow"""
-#
-#     def __init__(self, usecase_cls, post_schema: Schema):
-#         self.UsecaseClass = usecase_cls  # attrs class
-#         self.post_schema: Schema = post_schema
-#
-#     def to_usecase(self, mapping):
-#         """Return a UsecaseClass() from an HTTP Request."""
-#
-#         try:
-#             usecase_dict = self.post_schema.load(mapping).data
-#         except ValidationError as e:
-#             errors = self._handle_invalid_parameters(e.messages)
-#             raise self.InvalidMapping(errors)
-#
-#         return self.UsecaseClass(**usecase_dict)
+import attr
+from marshmallow import Schema, ValidationError
+
+
+class BaseJSONUsecaseAdapter:
+    """Class to marshal JSON into attrs classes using marshmallow"""
+
+    def __init__(self, usecase_cls, post_schema: Schema):
+        self.UsecaseClass = usecase_cls  # attrs class
+        self.post_schema: Schema = post_schema
+
+    def to_usecase(self, mapping):
+        """Return a UsecaseClass() from an HTTP Request."""
+
+        try:
+            usecase_dict = self.post_schema.load(mapping).data
+        except ValidationError as e:
+            # TODO see what this error looks like, get useful info out, and re-raise as as a different error
+            pass
+
+        return self.UsecaseClass(**usecase_dict)
---
+++ app/infrastructure/server/http/handlers/handler_factory.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiohttp import web
+
+"""This is the most opinionated part of the skeleton app
+
+Our adapters provide generalized methods for marshaling and un-marshaling objects
+and our database clients provide the same generality for database CRUD operations.
+The handler is the only skeleton functionality that dictates when and how we use 
+the those adapters and clients, and is the only place where web requests and
+database operations are explicitly tied together
+Of course, it's also what makes the skeleton just *work* out of the box.
+"""
+
+
+def post_handler_factory(usecase_class) -> asyncio.coroutine:
+    """Create post handler coroutine to be called by aiohtttp upon receipt of a POST request"""
+
+    async def post_handler(request: web.Request) -> web.Response:
+        pass
+
+    return post_handler
